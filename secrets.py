af6d0ddd63bc38729a04a2f78b2a396b24f7b6c1
Rodrigo Arce C <rodrigoar87@gmail.com>
2025-05-19 10:28:38 -0600

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -2,5 +2,5 @@ secrets = {
     'ssid' : 'Lab Innovacion 2G',# wifi
     'password' : 'innovalab22',#password wifi
     'aio_username' : 'Eydan',
-    'aio_key' : 'aio_SIsR68e8kUSoc4zD8YquIywwBWtd'
+    'aio_key' : 'inser aqui su llave de adafruit AIO'
 }
