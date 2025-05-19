d2ffd3b9fa3d373f0ba14b0465b557abb2b1b740
Rodrigo Arce C <rodrigoar87@gmail.com>
2025-05-19 10:27:57 -0600

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,6 @@
+secrets = {
+    'ssid' : 'Lab Innovacion 2G',# wifi
+    'password' : 'innovalab22',#password wifi
+    'aio_username' : 'Eydan',
+    'aio_key' : 'aio_SIsR68e8kUSoc4zD8YquIywwBWtd'
+}
